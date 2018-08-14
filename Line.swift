c49357b379867ee9a166a5b15810c62d5de4b85a
DK <dwkim16@gmail.com>
2018-08-14 14:23:21 +0900

    - ch. 19

---
+++ Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Dowon Kim on 8/14/18.
+//  Copyright © 2018 Do Won Kim. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
